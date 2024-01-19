80b76bee97ca177d23ec36a5fd0f3c4ef7f2a9ad
Ulfy1 <yaroslav.zix@gmail.com>
2024-01-19 11:42:47 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+FILEPATH = 'notes.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(arg_todos, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(arg_todos)
+
